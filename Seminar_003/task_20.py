ac82bb0c9650bf00a1e020fbba49f0f300d670c6
TatsianaPutruk <tushkanchik85@gmail.com>
2023-04-20 12:17:15 +0300

    task_20.py added

--- Seminar_003/task_20.py
+++ Seminar_003/task_20.py
@@ -9,3 +9,41 @@
 # *Пример:*
 # ноутбук
 #     12
+
+english_dictionary = {1:'AEIOULNSTR',
+                      2:'DG',
+                      3:'BCMP',
+                      4:'FHVWY',
+                      5:'K',
+                      8:'JX',
+                      10:'QZ'}
+russian_dictionary = {1:'АВЕИНОРСТ',
+                      2:'ДКЛМПУ',
+                      3:'БГЁЬЯ',
+                      4:'ЙЫ',
+                      5:'ЖЗХЦЧ',
+                      8:'ШЭЮ',
+                      10:'ФЩЪ'}
+
+eng = 'qwertyuiopasdfghjklzxcvbnm'
+rus = 'йцукенгшщзхъфывапролджэячсмитьбюё'
+word = input('Введите слово -> ').upper()
+# if n == 1:
+# 	print('За это слово вы получаете', sum([k for i in word for k, v in english_dictionary.items() if i in v]), 'очков')
+# elif n == 2:
+#  	print('За это слово вы получаете', sum([k for i in word for k, v in russian_dictionary.items() if i in v]), 'очков')
+
+if word[0] in eng:
+    sum = 0
+    for letter in word:
+        for key, value in english_dictionary.items():
+            if letter in value: 
+                sum += key
+    print(f'Стоимость слова = {sum}')
+elif word[0].lower() in rus:
+    sum = 0
+    for letter in word:
+        for key, value in russian_dictionary.items():
+            if letter in value:
+                sum += key
+    print(f'Стоимость слова = {sum}')
