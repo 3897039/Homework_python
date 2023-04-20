716d614a748bd7b128f6ae62a72d4a73d3578170
TatsianaPutruk <tushkanchik85@gmail.com>
2023-04-20 11:27:27 +0300

    task_18.py added

--- Seminar_003/task_18.py
+++ Seminar_003/task_18.py
@@ -7,3 +7,21 @@
 #     1 2 3 4 5
 #     6
 #     -> 5
+
+import random
+n = int(input("Ведите количество элементов в массиве -> "))
+print(n)
+arrayA = [random.randint(0, n) for i in range(n)]
+print(arrayA)
+number_x = int(input("Введите некоторое число Х -> "))
+print(number_x)
+nearby_min = abs(number_x - arrayA[0])
+index = 0
+for i in range(1, n):
+    count = abs(number_x - arrayA[i])
+    if count < nearby_min:
+        nearby_min = count
+        index = i
+print(f'Число {arrayA[index]} в массиве A наиболее близко по величине к числу {number_x}')
+        
+
