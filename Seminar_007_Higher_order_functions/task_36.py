a5cc87eae9077415dee1ebfcb443a8bd21fde870
TatsianaPutruk <tushkanchik85@gmail.com>
2023-05-20 16:45:05 +0300

    task 36 added

--- Seminar_007_Higher_order_functions/task_36.py
+++ Seminar_007_Higher_order_functions/task_36.py
@@ -17,3 +17,9 @@
 # 6 12 18 24 30 36
 
 
+def print_operation_table(operation, num_rows = 6, num_columns = 6):
+    a = [[operation(i, j) for j in range(1, num_columns + 1)] for i in range(1, num_rows + 1)]
+    for i in a:
+        print(*[f"{x:>3}" for x in i])
+
+print_operation_table(lambda x, y: x * y)
